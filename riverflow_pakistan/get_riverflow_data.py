b23f4525d70c126ea3774d7594e1061e15c66afa
Steven-Mugisha <120755332+Steven-Mugisha@users.noreply.github.com>
2023-04-17 16:41:21 -0400

    Update get_riverflow_data.py

--- riverflow_pakistan/get_riverflow_data.py
+++ riverflow_pakistan/get_riverflow_data.py
@@ -3,77 +3,74 @@
     Author - Steven Mugisha Mizero
 """
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup as bs
 import pandas as pd
-import numpy as np
-import requests, io
-from bs4 import BeautifulSoup as bs 
-from tqdm import tqdm
-import json
-
-def crawl_riverflow(): 
-    wapdaUrl = "http://www.wapda.gov.pk/index.php/river-flow-data"
-    page = requests.get(wapdaUrl)
-
-    soup = bs(page.content, "html.parser")
-
-    table =  soup.find("table")
-
-    # Extract the data from the table
-    data = []
-    for row in table.find_all('tr'):
-        row_data = []
-        for cell in row.find_all('td'):
-            row_data.append(cell.text.strip())
-        data.append(row_data)
-
-    # # print(riverflowList)
-    columnsNames = ["Date","LEVEL (FEET) INDUS AT TARBELA","INFLOW INDUS AT TARBELA", "OUTFLOW INDUS AT TARBELA",
-                    "INFLOW KABUL AT NOWSHERA", "LEVEL (FEET) JEHLUM AT MANGLA", "INFLOW JEHLUM AT MANGLA",
-                    "OUTFLOW JEHLUM AT MANGLA", "INFLOW CHENAB AT MARALA", "CURRENT YEAR", "LAST YEAR", "AVG: Last 10-years" ]
-
-    riverflow_df = pd.DataFrame(columns=columnsNames)
+from datetime import datetime
 
 
-    for row in data[4:]:
-        riverflow_df.loc[len(riverflow_df)] = row
+def scrape_wapda_riverflow_data():
+    driver_path = "/Users/smugisha/Desktop/chromedriver_mac64/chromedriver"
+    service = Service(driver_path)
 
-    return riverflow_df
+    wapdaUrl = "https://www.wapda.gov.pk/river-flow"
 
-def main_rivers():
-    riverflow_df = crawl_riverflow()
-    inflow_df = riverflow_df[["Date","INFLOW INDUS AT TARBELA", "INFLOW KABUL AT NOWSHERA",
-                                               "INFLOW JEHLUM AT MANGLA", "INFLOW CHENAB AT MARALA"]]
-    
-    split_index = inflow_df.index.get_loc((inflow_df['Date'] == '1-Jan').idxmax())
-                                           
-    # dataframe corresponding to the year of 2022
-    rf_2022_df = inflow_df.loc[split_index + 1:].copy()
-    dates_2022= [s.replace('/', '-') if '/' in s else s for s in rf_2022_df.Date]
-    new_rf_2022_df= rf_2022_df.iloc[:,1:]
-    new_rf_2022_df["Date"]= dates_2022
-    rf_2022_reversed= new_rf_2022_df[::-1]
-    final_2022_df= rf_2022_reversed.set_index("Date")
-    final_2022_df.index = [pd.to_datetime(date_str + '-2022', format='%d-%b-%Y').strftime('%Y-%m-%d') for date_str in final_2022_df.index]
+    with webdriver.Chrome(service=service) as driver:
+        driver.get(wapdaUrl)
+        html = driver.page_source
 
-    # dataframe corresponding to the year of 2023
-    rf_2023_df = inflow_df.loc[:split_index].copy()
-    dates_2023= [s.replace('/', '-') if '/' in s else s for s in rf_2023_df.Date]
-    new_rf_2023_df= rf_2023_df.iloc[:,1:]
-    new_rf_2023_df["Date"]= dates_2023
-    rf_2023_reversed= new_rf_2023_df[::-1]
-    final_2023_df= rf_2023_reversed.set_index("Date")
-    final_2023_df.index= [pd.to_datetime(date_str + '-2023', format='%d-%b-%Y').strftime('%Y-%m-%d') for date_str in final_2023_df.index]
+    soup = bs(html, "html.parser")
 
-    # combined dataframe for 2022 and 2023
-    Inflow_combined_df= pd.concat([final_2022_df, final_2023_df], axis=0)
-    Inflow_combined_df.reset_index(inplace=True)
-    columns_names= ["Date","INDUS", "KABUL", "JEHLUM", "CHENAB"]
-    Inflow_combined_df.columns= columns_names
+    table = soup.find("table")
 
-    Inflow_combined_df['Year']= [year.split("-")[0] for year in list(Inflow_combined_df.Date)]
-    Inflow_combined_df.to_csv("mainInflowrivers.csv", index=False)
-    
-    return print("Done")
+    if table is not None:
+        # Extract data from the table
+        table_data = []
+        for row in table.find_all('tr'):
+            row_values = []
+            for cell in row.find_all('td'):
+                row_values.append(cell.text.strip())
+            if row_values:
+                table_data.append(row_values)
+
+        if table_data:
+            columnsNames = ["Date", "LEVEL (FEET) INDUS AT TARBELA", "INFLOW INDUS AT TARBELA", "OUTFLOW INDUS AT TARBELA",
+                            "INFLOW KABUL AT NOWSHERA", "LEVEL (FEET) JEHLUM AT MANGLA", "INFLOW JEHLUM AT MANGLA",
+                            "OUTFLOW JEHLUM AT MANGLA", "INFLOW CHENAB AT MARALA", "CURRENT YEAR", "LAST YEAR", "AVG: Last 10-years"]
+            output_table = pd.DataFrame(columns=columnsNames)
+
+            for row in table_data[:]:
+                output_table.loc[len(output_table)] = row
+
+            # Match the riverflow_df with the recentYearsRiverFlow_df
+            currentYear = datetime.now().year
+            riverflow_subset = output_table[["Date","INFLOW INDUS AT TARBELA", "INFLOW KABUL AT NOWSHERA", "INFLOW JEHLUM AT MANGLA", "INFLOW CHENAB AT MARALA"]]
+            columns_names = ["Date","indus_at_tarbela (cfs)", "kabul_at_nowshera (cfs)", "jhelum_at_mangal (cfs)", "cheanab_at_marala (cfs)"]
+            riverflow_subset.columns = columns_names
+            riverflow_subset["Date"] = riverflow_subset["Date"].apply(lambda x: x.replace(" ", "-"))
+            riverflow_subset["Date"] = [date_str + f'-{currentYear}' for date_str in riverflow_subset["Date"]]
+            riverflow_subset["Date"] = [pd.to_datetime(date_str, format='%d-%b-%Y').strftime('%Y-%m-%d') for date_str in riverflow_subset["Date"]]
+            riverflow_subset_reversed = riverflow_subset.iloc[::-1]
+
+            # Combine the two dataframes and save the current
+            recentYearsRiverFlow_df = pd.read_csv("recentYearsRiverFlow.csv")
+
+            # check the last date in the recentYearsRiverFlow_df
+            lastDate = recentYearsRiverFlow_df["Date"].iloc[-1]
+            if lastDate != riverflow_subset_reversed["Date"].iloc[-1]:
+                # append all values after the lastDate
+                data_to_append = riverflow_subset_reversed[riverflow_subset_reversed["Date"] > lastDate]
+                recentYearsRiverFlow_df = recentYearsRiverFlow_df.append(data_to_append, ignore_index=True)
+                recentYearsRiverFlow_df.to_csv("recentYearsRiverFlow.csv", index=False)
+            else:
+                # do nothing
+                print("No new data to append")
+
+        else:
+            print("Table is empty")
+    else:
+        print("Table not found")
 
 if __name__ == "__main__":
-    print(main_rivers())
+    scrape_wapda_riverflow_data()
