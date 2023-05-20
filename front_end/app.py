468eff05c85eaffb72bc9b89dd2603e8cf0c8197
smizero <steven.mizero@contech.univeristy>
2023-05-20 16:49:50 -0400

    app.py needs somemore work

--- front_end/app.py
+++ front_end/app.py
@@ -3,33 +3,22 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 from flow_percentiles import create_percentile_dataframe
+import logging
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)  # Set the desired logging level
+logger = logging.getLogger(__name__)
 
 st.set_page_config(layout="wide")
 
-
-
 st.title("RiverFlow hydrographs of main rivers in Pakistan")
 
-# st.sidebar.info(
-#     """
-#         - Data were downoloaded here: <http://www.wapda.gov.pk/index.php/river-flow-data>
-    
-#     """
-# )
-@st.cache_data
-def get_data(station:str) -> pd.DataFrame:
-    riverflow_df = pd.read_csv("/Users/mugisha/Desktop/clone/Drought_Pakistan/riverflow_pakistan/flows/"+station+".csv")
-    # name_rivers = ['INDUS', 'KABUL', 'JEHLUM', 'CHENAB']
-    # years = ["All Years","2022","2023"]
-    return riverflow_df
-
-riverflow_df = get_data("indus_at_tarbela")
-
-col1, col2, col3 = st.columns([.3, .15, 1.5])
-
+col1, col2, col3 = st.columns([.3, .3, 1.5])
 
 # Varaibles for selection: 
 name_rivers = ['Chenab_at_Marala','Indus_at_Tarbela', 'Jhelum_at_Mangla', 'Kabul_at_Nowshera']
+recent_years = ["Select Year",2010,2011]
 with col1:
     
     selected_station = st.selectbox(
@@ -37,6 +26,36 @@ with col1:
         name_rivers,
         index = name_rivers.index(name_rivers[0])
     )
+
+with col2:
+    selected_year = st.selectbox(
+        "Select Year",
+        recent_years,
+        index = recent_years.index(recent_years[0])
+    )
+
+@st.cache_data
+def get_data(selected_station) -> pd.DataFrame:
+    riverflow_df = pd.read_csv("/Users/mugisha/Desktop/clone/Drought_Pakistan/riverflow_pakistan/flows/"+selected_station+".csv")
+    riverflow_df =  riverflow_df[riverflow_df["Year"] == selected_year]
+    return riverflow_df
+
+if selected_year != "Select Year":
+    riverflow_df = get_data(selected_station)
+    print(riverflow_df)
+# else:
+#     logger.info(" !!!! You can get to me !!!")
+
+# print(riverflow_df)
+
+# @st.cache_data
+# def selected_year_df(df,year):
+#     df["Year"] = df["Year"].astype(str)
+#     filtered_df = df[df["Year"] == year]
+#     return filtered_df
+
+# riverflow_df = get_data(selected_station)
+
 
 try:
     Plot_dataframe = create_percentile_dataframe(selected_station)
@@ -88,50 +107,22 @@ try:
     # Create the figure object
     fig = go.Figure(data=traces, layout=layout)
 
+
+    # # Add the new line plot
+    # Line_dataframe = df_ToPlot
+    # traces.append(go.Scatter(
+    #     x="Date",
+    #     y=selected_station,  # Replace 'column_name' with the actual column you want to plot
+    #     line=dict(color='black')  # Customize the line color if needed
+    # ))
+    # fig.add_trace(traces[-1])
+
+
+
     # st.plotly_chart(fig)
     container =st.container()
     with container:
-        st.plotly_chart(fig, use_container_width=True, width=1, height=6)
+        st.plotly_chart(fig, use_container_width=True, width=1, height=0)
 
 except ValueError as e:
     st.error(str(e))
-
-
-
-
-# with col2:
-#     selected_year = st.selectbox(
-#         "Select Year",
-#         years,
-#         index=years.index("All Years")
-#     )
-
-
-# @st.cache_data
-# def get_filtered_data(df,year):
-#     df["Year"] = df["Year"].astype(str)
-#     filtered_df = df[df["Year"] == year]
-#     return filtered_df
-
-# if selected_year != "All Years":
-#     df_ToPlot = get_filtered_data(riverflow_df, selected_year)
-# else:
-#     df_ToPlot = riverflow_df
-
-# try:
-#     # fig = px.area(df_ToPlot, x="Date", y=selected_station, title=selected_station, color="Year",
-#     #               color_discrete_sequence=['darkorange'])
-#     fig = px.line(df_ToPlot, x="Date", y=selected_station, title=selected_station, color="Year")
-#     fig.update_traces(line_width=3)
-#     fig.update_layout(
-#         width=1000,
-#         height=500,
-#         yaxis_title="Riverflow in cfs",
-#         margin=dict(l=50, r=50, t=50, b=50),
-#         xaxis=dict(tickfont=dict(size=16)),
-#         yaxis=dict(tickfont=dict(size=16))
-#     )
-#     st.plotly_chart(fig)
-
-# except ValueError as e:
-#     st.error(str(e))
