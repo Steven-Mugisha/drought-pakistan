d95a974edbeade202d37eebd5d5bbb7158e625ab
smizero <steven.mizero@contech.univeristy>
2023-08-03 22:56:35 -0400

    updated get_riverflow_data

--- riverflow_pakistan/get_riverflow_data.py
+++ riverflow_pakistan/get_riverflow_data.py
@@ -18,12 +18,12 @@ logger = logging.getLogger(__name__)
 def create_dataframe() -> pd.DataFrame:
     """ This function creates a dataframe with the following columns: """
     
-    columnsNames = ["Date", "LEVEL (FEET) INDUS AT TARBELA", "INFLOW INDUS AT TARBELA", "OUTFLOW INDUS AT TARBELA",
+    columns_names = ["Date", "LEVEL (FEET) INDUS AT TARBELA", "INFLOW INDUS AT TARBELA", "OUTFLOW INDUS AT TARBELA",
                         "INFLOW KABUL AT NOWSHERA", "LEVEL (FEET) JEHLUM AT MANGLA", "INFLOW JEHLUM AT MANGLA",
                             "OUTFLOW JEHLUM AT MANGLA", "INFLOW CHENAB AT MARALA",
                                  "CURRENT YEAR", "LAST YEAR", "AVG: Last 10-years"]
     
-    output_table = pd.DataFrame(columns=columnsNames)
+    output_table = pd.DataFrame(columns=columns_names)
     return output_table
 
 
@@ -47,9 +47,9 @@ def year_specific_dataframe(output_table,year) -> pd.DataFrame:
     return out_put_year_df
 
 
-def get_riverflow_table(url, year) -> pd.DataFrame:
+def get_year_riverflow_table(url, year) -> pd.DataFrame:
 
-    """ reads the data from the website and returns a dataframe with the following columns:"""
+    """ reads the data from the website and returns a dataframe for a specific year."""
 
     # defining selenium variables:
     chrome_options = Options()
@@ -61,27 +61,28 @@ def get_riverflow_table(url, year) -> pd.DataFrame:
 
         try:
             select_element = driver.find_element("css selector", ".MuiSelect-root.MuiSelect-select.MuiSelect-selectMenu.MuiSelect-outlined.MuiInputBase-input.MuiOutlinedInput-input")
-            logger.info("***------------Select element found.------------***")
+            logger.info("Select element found.")
 
             # I want to see the options in the select element and log them out:
             select_element.click()
             time.sleep(1)
             select_options = driver.find_elements("css selector", ".MuiButtonBase-root.MuiListItem-root.MuiMenuItem-root.MuiMenuItem-gutters.MuiListItem-gutters.MuiListItem-button")
-            logger.info("***------------Select options found.------------***")
+            logger.info("Select options found.")
             for option in select_options:
                 # if option.text is equal to current year, then click it:
                 if option.text == str(year):
                     option.click()
-                    logger.info("***------------Select option clicked.------------***")
+                    logger.info(f"Selected the year: {year} option clicked")
                     time.sleep(1)
                     # Now I want to get the table data:
                     page_source_overview = driver.page_source
                     soup = bs(page_source_overview, "html.parser")
                     time.sleep(5)
                     table = soup.find("table")
-                    logger.info("***------------Table found.------------***")
+                    logger.info("------------ Table found ------------")
 
                     if table:
+                        # the list of lists that will contain the table data
                         table_data = []
                         for row in table.find_all('tr'):
                             row_values = []
@@ -90,23 +91,23 @@ def get_riverflow_table(url, year) -> pd.DataFrame:
                             if row_values:
                                 table_data.append(row_values)
                             else:
-                                logger.info("***------------Table data not found.------------***")
+                                logger.info("------------Table data not found ------------")
                     else:
-                        logger.info("***------------Table not found.------------***")
+                        logger.info("------------Table not found ------------ ")
                     
                     if table_data:
                         output_table = create_dataframe()
                         for row in table_data[:]:
                             output_table.loc[len(output_table)] = row
                             output_table = output_table.iloc[::-1]
-                        logger.info(f"***----THE LENGTH OF THE TABLE ==== {len(output_table)} ----***")
-                        # logger.info(f"***------------Table data extracted.------------***{output_table}")
+                        logger.info(f" THE LENGTH OF THE TABLE {len(output_table)}")
+        
                    
                 else:
-                    logger.info("***------------Select option not clicked.------------***")
+                    logger.info("Select option not clicked")
 
         except Exception as e:
-            logger.error(f"***------------Error: {e}------------***")
+            logger.error(f"------------Error: {e}------------")
         
         return output_table
 
@@ -116,89 +117,83 @@ def individual_year_data(url, threshold_days = 60):
     current_year = datetime.now().year
     previous_year = current_year - 1
 
-    # to check which days are missing in any from the dataframe: 
+    # to check which days missing from the dataframe: 
     today = datetime.now()
     start_date = datetime(today.year, 1, 1)
     # The number of days since 1st of January:
     delta = today - start_date
+    logger.info(f" ------------ The number of days since 1st of January: {delta.days} ------------ ")
     
-    # all existing data:
-    recentYearsRiverFlow_df = pd.read_csv("recentYearsRiverFlow.csv")
+
+    # the existing dataset:
+    recentYearsRiverFlow_df = pd.read_csv("/Users/mugisha/Desktop/clone/Drought_Pakistan/riverflow_pakistan/recentYearsRiverFlow.csv")
     # set the index to be the date column:
     recentYearsRiverFlow_df.set_index("Date", inplace=True)
 
     if current_year:
-        current_year_table = get_riverflow_table(url, current_year)
+        current_year_table = get_year_riverflow_table(url, current_year)
 
-        if  len(current_year_table) > threshold_days & len(current_year_table) == delta.days:
-            logger.info("***------------Table data is equal to the number of days since 1st of January.------------***")
+        # if  (len(current_year_table) > threshold_days) & (len(current_year_table) == delta.days):
+        if  (len(current_year_table) > threshold_days):
             current_year_table = year_specific_dataframe(current_year_table, current_year)
-            # select the last index in the dataframe:
+            # select the last index in the old and new dataframes:
             lastDate_newdata = current_year_table.index[-1]
-
-            # lastDate_newdata = current_year_table.iloc[-1]
-            logger.info(f"------------------Last date of new data is held here: {lastDate_newdata}-------------")
-
             lastDate = recentYearsRiverFlow_df.index[-1]
-            logger.info(f"****--------------Last date of old data is held here: {lastDate}----------****")
 
             if lastDate != lastDate_newdata:
-                lastDate_newdata = pd.to_datetime(lastDate_newdata)
-                threshold_days_before = lastDate_newdata - pd.Timedelta(days=threshold_days)
-                threshold_days_before  = threshold_days_before.strftime('%Y-%m-%d')
-                recentYearsRiverFlow_df = recentYearsRiverFlow_df[recentYearsRiverFlow_df.index < threshold_days_before]
-                data_to_append = current_year_table[current_year_table.index >= threshold_days_before]
+                before_threshold_day = pd.to_datetime(lastDate) - pd.Timedelta(days=threshold_days)
+                before_threshold_day = before_threshold_day.strftime('%Y-%m-%d')
+                recentYearsRiverFlow_df = recentYearsRiverFlow_df[recentYearsRiverFlow_df.index < before_threshold_day]
+                data_to_append = current_year_table[current_year_table.index > before_threshold_day]
                 # concat the dataframes:
                 recentYearsRiverFlow_df = pd.concat([recentYearsRiverFlow_df, data_to_append], axis=0)
+                logger.info(" ------------ Dataframes updated. ------------ ")
 
-                logger.info("***------------Data appended to the recentYearsRiverFlow_df.------------***")
-                logger.info(f"***----{recentYearsRiverFlow_df} ----***")
 
                 # saving the data to csv:
-                recentYearsRiverFlow_df.to_csv("recentYearsRiverFlow.csv")
+                recentYearsRiverFlow_df.to_csv("/Users/mugisha/Desktop/clone/Drought_Pakistan/riverflow_pakistan/recentYearsRiverFlow.csv")
+                logger.info(" ------------ Data saved to csv. ------------ ")
     
             else:
-                logger.info("***------------Table data is not equal to the number of days since 1st of January.------------***")
+                logger.info("Table data is not equal to the number of days since 1st of January")
         
-        elif len(current_year_table) < threshold_days & len(current_year_table) == delta.days:
-            logger.info("***------------Table data is equal to the number of days since 1st of January.------------***")
-            logger.info("***------------ The current year doesn't have enough data defined by the threshold day.------------***")
-            previous_year_table = get_riverflow_table(url, previous_year)
+        elif len((current_year_table) < threshold_days):
+            logger.info("Data available is less than the threshold days but equal to the number of days since 1st of January")
+            logger.info(f" The current year table length is: {len(current_year_table)}")
+            logger.info("Get the previous year data to fill the gap")
+
+            previous_year_table = get_year_riverflow_table(url, previous_year)
             previous_year_table = year_specific_dataframe(previous_year_table, previous_year)
             lastDate_newdata = previous_year_table.index[-1]
-            logger.info(f"------------------Last date of new data is held here: {lastDate_newdata}-------------")
+            logger.info(f" ------------------ Last date of new data is held here: {lastDate_newdata} ------------- ")
 
             days_to_get_from_previous_year = threshold_days - len(current_year_table)
-            logger.info(f"------------------Days to get from previous year: {days_to_get_from_previous_year}-------------")
+
             # get the last rows of the previous year table equal to the days_to_get_from_previous_year:
+            logger.info(f" Reading the last year table to match the threshold days")
             previous_year_table = previous_year_table.tail(days_to_get_from_previous_year)
-            logger.info(f"------------------ I am reading the curent table to also get the available data -------------")
-            current_year_table = year_specific_dataframe(current_year_table, current_year)
-            logger.info(f"------------------ I am appending the previous year table to the current year table -------------")
+           
+            # concat the two dataframes:
             combined_table = pd.concat([previous_year_table, current_year_table], axis=0)
             lastDate_combined_table = combined_table.index[-1]
             lastDate = recentYearsRiverFlow_df.index[-1]
 
-            if lastDate != lastDate_combined_table:
-                lastDate_combined_table = pd.to_datetime(lastDate_combined_table)
-                threshold_days_before = lastDate_combined_table - pd.Timedelta(days=threshold_days)
-                threshold_days_before  = threshold_days_before.strftime('%Y-%m-%d')
-                recentYearsRiverFlow_df = recentYearsRiverFlow_df[recentYearsRiverFlow_df.index < threshold_days_before]
-                data_to_append = combined_table[combined_table.index >= threshold_days_before]
-                recentYearsRiverFlow_df = recentYearsRiverFlow_df.append(data_to_append, ignore_index=True)
-                logger.info("***------------Data appended to the recentYearsRiverFlow_df.------------***")
-                logger.info(f"***----{recentYearsRiverFlow_df} ----***")
+            # Then get the last rows of the combined table equal to the threshold days:
+            before_threshold_day = pd.to_datetime(lastDate) - pd.Timedelta(days=threshold_days)
+            before_threshold_day = before_threshold_day.strftime('%Y-%m-%d')
+            recentYearsRiverFlow_df = recentYearsRiverFlow_df[recentYearsRiverFlow_df.index < before_threshold_day]
+            data_to_append = combined_table[combined_table.index > before_threshold_day]
+            recentYearsRiverFlow_df = pd.concat([recentYearsRiverFlow_df, data_to_append], axis=0)
+            logger.info(" ------------ Dataframes updated. ------------ ")
             
-            else:
-                logger.info("***------------Table data is not equal to the number of days since 1st of January.------------***")
-            
+
             # saving the data to csv:
-            recentYearsRiverFlow_df.to_csv("recentYearsRiverFlow.csv")
+            recentYearsRiverFlow_df.to_csv("/Users/mugisha/Desktop/clone/Drought_Pakistan/riverflow_pakistan/recentYearsRiverFlow.csv")
         else:
-            logger.info("***------------Table data is not equal to the number of days since 1st of January.------------***")
+            logger.info("No data at all")
                 
     else:
-        logger.info("***------------Current year not found.------------***")
+        logger.info(" ------------ Current year not found. ----------- ")
     
     return current_year_table
 
